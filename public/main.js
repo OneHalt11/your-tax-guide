83aa54d5f5e3404fab574958ff42aa1a8da87386
OneHalt11 <126759085+OneHalt11@users.noreply.github.com>
2023-03-02 15:20:59 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
 document.getElementById('link-to-buy').addEventListener('click', function() {
-    // ga('send', 'event', 'link', 'click', 'example.com');
+        ga('send', 'event', 'link', 'click', 'buy_full_course_cta', 499);
     console.log("CTA clicked")
   });  
