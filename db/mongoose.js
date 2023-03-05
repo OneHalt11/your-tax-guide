ab3727b3653a0d3e9d279c0dc993027adeece7e9
Harshit Kumar <harshit1kumar@gmail.com>
2023-03-05 13:04:21 +0530

    Removed local DB

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/your-tax-guide', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
