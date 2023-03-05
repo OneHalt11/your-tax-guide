5d02ce3df2c5e793d538377ae400afe6f216050a
Harshit Kumar <harshit1kumar@gmail.com>
2023-03-05 13:18:55 +0530

    Mongoose connection revamped

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,5 +2,14 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
+
+const conn = mongoose.connection;
+
+conn.on('error', () => console.error.bind(console, 'connection error'));
+
+conn.once('open', () => console.info('Connection to Database is successful'));
+
+module.exports = conn;
